88535f2d7f26f0e5f0ee5018dbbddc43d8ac445c
Behruz <behruzparpiyev20@gmail.com>
2022-07-20 13:59:07 +0500

    .....

--- blogs/views.py
+++ blogs/views.py
@@ -3,8 +3,14 @@ from django.views.generic import ListView, DetailView
 from .models import PostModel
 
 class PostListView(ListView):
-    queryset = PostModel.objects.order_by('-id')
     template_name = 'blog.html'
+
+    def get_queryset(self):
+        qs =  PostModel.objects.order_by('-id')
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tag__name=tag)
+        return qs    
 
 class PostDetailView(DetailView):
     model = PostModel
