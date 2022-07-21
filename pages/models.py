effc458bf2897efad1ce6e33c7ae10ded9ba24c2
Behruz <behruzparpiyev20@gmail.com>
2022-07-21 14:48:09 +0500

    pages banner done

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,7 @@
+from distutils.command.upload import upload
+from doctest import debug_script
 from tabnanny import verbose
+from typing import Collection
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -14,4 +17,22 @@ class ContactModel(models.Model):
 
     class Meta:
         verbose_name = 'contact' 
-        verbose_name_plural = 'contacts'   
+        verbose_name_plural = 'contacts'  
+
+
+
+class BannerModel(models.Model):
+    collection = models.CharField(max_length=60, verbose_name=_('collection'))
+    title = models.CharField(max_length=60, verbose_name=_('title'))
+    description = models.CharField(max_length=100, verbose_name=_('description'))
+    image = models.ImageField(upload_to='banners/', verbose_name=_('image'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
+    is_active = models.BooleanField(default=False, blank=True ,verbose_name=_('is active'))
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'banner' 
+        verbose_name_plural = 'banners'   
+
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, reverse
 from django.views.generic import TemplateView, CreateView
 from blogs.models import PostModel
-
 from pages.forms import ContactModelForm
+from .models import BannerModel
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
@@ -10,6 +10,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         data['posts'] = PostModel.objects.order_by('-pk')[:3]
+        data['banners'] = BannerModel.objects.filter(is_active=True).order_by('-id')
         return data
 
 
