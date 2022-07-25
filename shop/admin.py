fe8052251e2928ae10879942b30a642e3135f2db
Behruz <behruzparpiyev20@gmail.com>
2022-07-25 20:01:37 +0500

    added shop forms

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import ProductTagModel, ProductModel, CategoryModel, BrandModel, ColorModel, SizeModel
+from .forms import ColorModelAdminForms
 
 @admin.register(ProductTagModel)
 class ProductTagModelAdmin(admin.ModelAdmin):
@@ -16,9 +18,15 @@ class SizeModelAdmin(admin.ModelAdmin):
 
 @admin.register(ColorModel)
 class ColorModelAdmin(admin.ModelAdmin):
-    list_display = ['code']
+    list_display = ['code', 'color']
     list_display_links = ['code']
-    search_fields = ['code']      
+    search_fields = ['code'] 
+    form = ColorModelAdminForms     
+
+
+    def color(self, obj):
+        free_space = '&nbsp;' * 5
+        return mark_safe(f"<div style='background-color:{obj.code}; width: 200px'>{free_space}</div>")
 
 @admin.register(CategoryModel)
 class CategoryModelAdmin(admin.ModelAdmin):
--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,3 @@
-
-from unicodedata import category, decimal
-from django.utils import timezone
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from ckeditor_uploader.fields import RichTextUploadingField
